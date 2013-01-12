d03f1c31fd27d338fe8e4d4554f10b1ca1ceacd1
Camilo Garzon <camilogc2@gmail.com>
2013-01-12 10:04:52 -0500

    HELLO, I'M TEDI

--- movil/src/co/qsystems/tedi/TEDI.java
+++ movil/src/co/qsystems/tedi/TEDI.java
@@ -1,22 +1,16 @@
 package co.qsystems.tedi;
 
+import org.apache.cordova.*;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class TEDI extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_tedi);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_tedi, menu);
-		return true;
-	}
-
+public class TEDI extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
 }
+
